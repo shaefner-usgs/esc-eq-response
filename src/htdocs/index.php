145e0e7c852361c03a11f97ae59709690a10f0c0
Scott Haefner <shaefner@usgs.gov>
2016-09-28 18:33:09 -0700

    initial commit

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,24 +1,38 @@
-<?php
-
-if (!isset($TEMPLATE)) {
-  // template functions
-  include_once 'functions.inc.php';
-
-  // defines the $CONFIG hash of configuration variables
-  include_once '../conf/config.inc.php';
-
-  $HEAD = '<link rel="stylesheet" href="css/index.css"/>';
-  $FOOT = '<script src="js/index.js"></script>';
-
-  include 'template.inc.php';
-}
-
-?>
-
-<div id="application">
-  <noscript>
-    <a href="https://www.google.com/search?q=javascript">
-      This page requires javascript.
-    </a>
-  </noscript>
-</div>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="/lib/leaflet-1.0.0/leaflet.css" />
+    <link rel="stylesheet" href="css/index.css" />
+  </head>
+  <body>
+    <ul class="modes">
+      <li><a href="#edit">Edit</a></li>
+      <li><a href="#map">Map</a></li>
+      <li><a href="#summary">Summary</a></li>
+    </ul>
+    <section id="edit" class="hide">
+      <h2>Edit</h2>
+      <form>
+        <label for="eqid">
+          Earthquake id:
+          <input type="text" id="eqid" />
+        <label>
+        <label for="mag">
+          Magnitude threshold:
+          <input type="text" id="mag" value="2.5" />
+        <label>
+      </form>
+    </section>
+    <section id="map" class="hide">
+      <h2>Map</h2>
+      <div class="map"></div>
+    </section>
+    <section id="summary" class="hide">
+      <h2>Summary</h2>
+    </section>
+  </body>
+  <script src="/lib/leaflet-1.0.0/leaflet.js"></script>
+  <script src="js/index.js"></script>
+</html>
--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -1,25 +1,11 @@
 'use strict';
 
-var View = require('mvc/View');
 
+var Controller = require('Controller'),
+    Map = require('map/Map');
 
-var view;
+Controller();
 
-view = View({
-  el: document.querySelector('#application')
-});
-
-view.render = function () {
-  view.el.innerHTML = view.model.get('content');
-};
-
-new Promise(function (resolve/*, reject*/) {
-  view.model.set({
-    content: 'waiting 2 seconds'
-  });
-  setTimeout(resolve, 2000);
-}).then(function () {
-  view.model.set({
-    content: 'js content'
-  });
+Map({
+  el: document.querySelector('.map')
 });
