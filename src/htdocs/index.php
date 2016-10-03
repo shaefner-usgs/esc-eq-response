18bcbdf5b3982a9ac8c8debe108c9339131343bc
Scott Haefner <shaefner@usgs.gov>
2016-10-03 16:12:21 -0700

    add meta tag

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css" />
     <link rel="stylesheet" href="css/index.css" />
