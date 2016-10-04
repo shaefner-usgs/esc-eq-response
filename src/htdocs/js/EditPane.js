609f50d54410026274e9255248e2582db8dccb02
Scott Haefner <shaefner@usgs.gov>
2016-10-04 14:08:54 -0700

    initial commit

---
+++ src/htdocs/js/EditPane.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+var EditPane = function (options) {
+  var _this,
+      _initialize;
+
+  _this = {};
+
+  _initialize = function () {
+
+  };
+
+  /*
+   * Get default values for form fields that depend on selected earthquake
+   *
+   * @param earthquake {Object}
+   *
+   * @return {Object}
+   */
+  _this.getDefaults = function (earthquake) {
+    var mag;
+
+    mag = earthquake.properties.mag;
+
+    return {
+      ashockDistance: Math.max(5, Math.round(mag - 2) * 5),
+      histDistance: Math.max(10, Math.round(mag - 2) * 10)
+    };
+  };
+
+
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+module.exports = EditPane;
