f38d1f15faa1cd94a9468af20125507692ff9427
Scott Haefner <shaefner@usgs.gov>
2016-10-06 11:20:46 -0700

    add mainshock to map

--- src/htdocs/js/Application.js
+++ src/htdocs/js/Application.js
@@ -2,6 +2,7 @@
 
 
 var Earthquake = require('Earthquake'),
+    EarthquakesLayer = require('features/EarthquakesLayer'),
     EditPane = require('EditPane'),
     MapPane = require('MapPane'),
     Navigation = require('Navigation');
@@ -29,7 +30,6 @@ var Application = function (options) {
       map: options.map,
       summary: options.summary
     };
-
     _eqid = document.getElementById('eqid');
 
     _editPane = EditPane();
@@ -46,12 +46,32 @@ var Application = function (options) {
     _eqid.addEventListener('change', _createEarthquake);
   };
 
+  /**
+   * Add earthquake (mainshock) to map
+   *
+   * @param geojson {Object}
+   *     Geojson layer returned by Earthquake class
+   */
   _addEarthquake = function (geojson) {
+    var coords,
+        mainshock;
+
     _earthquake = geojson;
+    coords = _earthquake.features[0].geometry.coordinates;
+    mainshock = EarthquakesLayer({
+      data: _earthquake
+    });
 
     _editPane.setDefaults(_earthquake);
+
+    _mapPane.map.addLayer(mainshock);
+    _mapPane.map.setView([coords[1], coords[0]], 9, true);
+    _mapPane.layerController.addOverlay(mainshock, 'Mainshock');
   };
 
+  /**
+   * Create a new earthquake instance using event id provided by user
+   */
   _createEarthquake = function () {
     Earthquake({
       callback: _addEarthquake,
