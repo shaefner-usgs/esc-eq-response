181ea8dcd9e2147abe6fafef56044b31bb351d14
Scott Haefner <shaefner@usgs.gov>
2016-10-11 15:08:02 -0700

    add summary; tweak symbology

--- src/htdocs/js/features/EarthquakesLayer.js
+++ src/htdocs/js/features/EarthquakesLayer.js
@@ -23,7 +23,7 @@ _COLORS = {
 _MARKER_DEFAULTS = {
   weight: 1,
   opacity: 0.5,
-  fillOpacity: 0.8,
+  fillOpacity: 0.7,
   color: '#333'
 };
 _DEFAULTS = {
@@ -52,8 +52,10 @@ var EarthquakesLayer = function (options) {
       _pastDay,
       _pastHour,
       _pastWeek,
+      _summary,
 
       _getAge,
+      _getSummary,
       _onEachFeature,
       _pointToLayer;
 
@@ -66,11 +68,15 @@ var EarthquakesLayer = function (options) {
     _pastDay = Moment.utc().subtract(1, 'days');
     _pastHour = Moment.utc().subtract(1, 'hours');
     _pastWeek = Moment.utc().subtract(1, 'weeks');
+    _summary = '';
 
     _this = L.geoJson(options.data, {
       onEachFeature: _onEachFeature,
       pointToLayer: _pointToLayer
     });
+
+    // Attach text summary to layer
+    _this.summary = _getSummary();
   };
 
 
@@ -104,6 +110,19 @@ var EarthquakesLayer = function (options) {
   };
 
   /**
+   * Get summary html for summary pane
+   *
+   * @return summary {String}
+   */
+  _getSummary = function () {
+    var summary;
+
+    summary = '<table>' + _summary + '</table>';
+
+    return summary;
+  };
+
+  /**
    * Leaflet GeoJSON option: called on each created feature layer. Useful for
    * attaching events and popups to features.
    *
@@ -114,32 +133,74 @@ var EarthquakesLayer = function (options) {
     var data,
         label,
         labelTemplate,
+        localTime,
+        momentObj,
         popup,
         popupTemplate,
-        props;
+        props,
+        summaryTemplate,
+        utcTime;
 
     props = feature.properties;
+
+    momentObj = Moment.utc(props.time, 'x');
+    utcTime = momentObj.format('MMM D, YYYY HH:mm:ss') + ' UTC';
+
+    // Calculate local time if tz prop included in feed; otherwise use UTC
+    if (props.tz) {
+      localTime = momentObj.utcOffset(props.tz).format('MMM D, YYYY h:mm:ss A');
+    } else {
+      localTime = utcTime;
+    }
+
     data = {
+      depth: feature.geometry.coordinates[2],
+      lat: feature.geometry.coordinates[1],
+      lng: feature.geometry.coordinates[0],
+      localTime: localTime,
       mag: Math.round(props.mag * 10) / 10,
-      time: Moment.utc(props.time, 'x').format('ddd, MMM D HH:mm:ss') + ' UTC',
+      magType: props.magType,
       place: props.place,
+      status: props.status,
+      utcTime: utcTime,
       url: props.url
     };
 
-    labelTemplate = 'M{mag} - {time}';
+    // Create label
+    labelTemplate = 'M{mag} - {utcTime}';
     label = L.Util.template(labelTemplate, data);
 
+    // Create popup html
     popupTemplate = '<div class="popup eq">' +
-        '<h2>M{mag}, {place}</h2>' +
-        '<time>{time}</time>' +
-        '<p><a href="{url}" target="_blank">Details</a> &raquo;</p>' +
+        '<h2><a href="{url}" target="_blank">M {mag} - {place}</a></h2>' +
+        '<dl>' +
+          '<dt>Time</dt><dd><time>{utcTime}</time></dd>' +
+          '<dt>Location (depth)</dt><dd>{lat}, {lng} ({depth} km)</dd>' +
+          '<dt>Magnitude ({magType})</dt><dd>{mag}</dd>' +
+          '<dt>Status</dt><dd>{status}</dd>' +
+        '</dl>' +
       '</div>';
     popup = L.Util.template(popupTemplate, data);
 
+    // Bind popup and label to marker
     layer.bindPopup(popup, {
       autoPanPadding: L.point(50, 50),
       maxWidth: '265'
     }).bindLabel(label);
+
+    // Create summary html
+    summaryTemplate = '<tr>' +
+        '<th>Mag</th>' +
+        '<th>Time <span>(local unless UTC specified)</span></th>' +
+        '<th>Cooordinates</th>' +
+        '<th>Depth</th>' +
+      '</tr><tr>' +
+        '<td>{magType} {mag}</td>' +
+        '<td>{localTime}</td>' +
+        '<td>{lat}, {lng}</td>' +
+        '<td>{depth} km</td>' +
+      '</tr>';
+    _summary += L.Util.template(summaryTemplate, data);
   };
 
   /**
