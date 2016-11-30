2e0b4c31eeac845a4e5b2c4d69a17e3bd03fab84
Scott Haefner <shaefner@usgs.gov>
2016-11-30 10:01:11 -0800

    pass MapPane instance to EditPane

--- src/htdocs/js/Application.js
+++ src/htdocs/js/Application.js
@@ -63,6 +63,7 @@ var Application = function (options) {
       el: options.edit,
       features: _features,
       loadingModule: _loadingModule,
+      mapPane: _mapPane,
       summaryPane: _summaryPane
     });
   };
