33416379f10925d7cec1a97a31fe1033ba2ff1b6
Scott Haefner <shaefner@usgs.gov>
2016-12-09 11:52:20 -0800

    switch to h4 tag for eq title

--- src/htdocs/js/Earthquake.js
+++ src/htdocs/js/Earthquake.js
@@ -167,8 +167,8 @@ var Earthquake = function (options) {
     latlng = AppUtil.round(coords[1], 3) + ', ' + AppUtil.round(coords[0], 3);
     mag = AppUtil.round(props.mag, 1);
 
-    html = '<h2><a href="' + props.url + '">' + props.magType + ' ' + mag +
-      ' - ' + props.place + '</a></h2>';
+    html = '<h4><a href="' + props.url + '">' + props.magType + ' ' + mag +
+      ' - ' + props.place + '</a></h4>';
     html += '<dl>' +
         '<dt>Time</dt>' +
         '<dd>';
