50e3e72d2b7c0ba418e522b0e5b19985c0cb5515
Scott Haefner <shaefner@usgs.gov>
2017-09-07 10:09:15 -0700

    lighten stroke color a bit

--- src/htdocs/js/PlotsPane.js
+++ src/htdocs/js/PlotsPane.js
@@ -276,7 +276,7 @@ var PlotsPane = function (options) {
       marker: {
         color: data.color, // fill
         line: {
-          color: 'rgb(0, 0, 0)' // stroke
+          color: 'rgb(51, 51, 51)' // stroke
         },
         size: data.size,
         sizeref: sizeref,
