10627f01333c79fcc3c4f8237e1e878b09eb2beb
Brad Aagaard <baagaard@usgs.gov>
2018-01-09 08:21:31 -0800

    Improve comment.

--- src/htdocs/js/EditPane.js
+++ src/htdocs/js/EditPane.js
@@ -245,7 +245,7 @@ var EditPane = function (options) {
       return false;
     }
 
-    // Check eqid format (2 letters followed by 5 characters)
+    // Check eqid format (2 letters followed by 5 or more characters)
     regex = /^[a-zA-Z]{2}[a-zA-Z0-9]{5,}$/;
     if (regex.test(_eqid.value)) {
       return true;
