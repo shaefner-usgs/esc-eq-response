ac553133f4c98de19e17da9dc34e273306c51756
Scott Haefner <shaefner@usgs.gov>
2018-07-11 16:28:58 -0700

    set custom icon, popup content for fieldnotes layer

--- src/htdocs/js/features/FieldNotesFeature.js
+++ src/htdocs/js/features/FieldNotesFeature.js
@@ -2,12 +2,37 @@
 'use strict';
 
 
+var Util = require('hazdev-webutils/src/util/Util');
+
+
+var _DEFAULTS,
+    _ICON_DEFAULTS,
+    _MARKER_DEFAULTS;
+
+_ICON_DEFAULTS = {
+  iconAnchor: L.point(10, 25),
+  iconRetinaUrl: '../img/pin-s-star+c0f@2x.png',
+  iconSize: L.point(20, 50),
+  iconUrl: '../img/pin-s-star+c0f.png',
+  popupAnchor: L.point(0, -25),
+  tooltipAnchor: L.point(0, -15)
+};
+_MARKER_DEFAULTS = {
+  opacity: 0.85
+};
+_DEFAULTS = {
+  iconOptions: _ICON_DEFAULTS,
+  json: {},
+  markerOptions: _MARKER_DEFAULTS
+};
+
 var FieldNotesFeature = function (options) {
   var _this,
       _initialize,
 
       _count,
       _mapLayer,
+      _markerOptions,
 
       _createPopupContent,
       _getName,
@@ -18,29 +43,43 @@ var FieldNotesFeature = function (options) {
   _this = {};
 
   _initialize = function (options) {
-    // Unique id; note that value is "baked into" app's js/css
-    var id = 'fieldnotes';
+    var iconOptions;
 
     options = options || {};
+    iconOptions = Util.extend({}, _ICON_DEFAULTS, options.iconOptions);
+
+    _this.displayLayer = false;
+    _this.id = 'fieldnotes'; // unique id; value is "baked into" app's js/css
+    _this.zoomToLayer = false;
 
     _count = 0;
-
-    _this.id = id;
+    _markerOptions = Util.extend({
+      icon: L.icon(iconOptions),
+      pane: _this.id
+    }, _MARKER_DEFAULTS, options.markerOptions);
 
     _mapLayer = L.geoJson(options.json, {
       onEachFeature: _onEachFeature,
       pointToLayer: _pointToLayer
     });
-    _mapLayer.id = id; // Attach id to L.Layer
+    _mapLayer.id = _this.id; // attach id to L.Layer
 
-    _this.displayLayer = false;
     _this.name = _getName();
-    _this.zoomToLayer = false;
-
   };
 
   _createPopupContent = function (feature) {
-    return feature.properties.form;
+    var html;
+
+    html = L.Util.template('<h3>{form}</h3>' +
+      '<p>{timestamp}</p>' +
+      '<p>{description}</p>' +
+      '<p>{notes}</p>' +
+      '<p>{site}</p>' +
+      '<p>{operator}</p>',
+      feature.properties
+    );
+
+    return html;
   };
 
   /**
@@ -85,9 +124,7 @@ var FieldNotesFeature = function (options) {
    * @return {L.CircleMarker}
    */
   _pointToLayer = function (feature, latlng) {
-    return L.marker(latlng, {
-      pane: _this.id
-    });
+    return L.marker(latlng, _markerOptions);
   };
 
   // ----------------------------------------------------------
