9020c811949ff57a50f254dfcda18453ad08281e
Scott Haefner <shaefner@usgs.gov>
2018-07-25 16:26:30 -0700

    exclude unneeded file that causes uglify to fail

--- gruntconfig/uglify.js
+++ gruntconfig/uglify.js
@@ -18,7 +18,8 @@ var uglify = {
       cwd: config.build + '/' + config.src,
       src: [
         '**/*.js',
-        '!**/bundle.js'
+        '!**/bundle.js',
+        '!**/leaflet-src.esm.js' // uglify fails and this file is not needed
       ],
       dest: config.dist
     }]
