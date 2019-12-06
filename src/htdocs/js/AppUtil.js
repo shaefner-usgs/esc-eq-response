3b1763d772262c7ceb528d15b473c0bd8deb0a3f
Scott Haefner <shaefner@usgs.gov>
2019-12-06 11:44:12 -0800

    add new method for inserting commas in large numbers

--- src/htdocs/js/AppUtil.js
+++ src/htdocs/js/AppUtil.js
@@ -14,6 +14,35 @@ AppUtil.LatLon = LatLon;
 AppUtil.Moment = Moment;
 
 /**
+ * Add commas to large numbers
+ *
+ * @param num {Number}
+ *
+ * @return {Number}
+ */
+AppUtil.addCommas = function (num) {
+  var regex,
+      x,
+      x1,
+      x2;
+
+  num += ''; // convert to string
+  regex = /(\d+)(\d{3})/;
+  x = num.split('.');
+  x1 = x[0];
+  x2 = '';
+
+  if (x.length > 1) {
+    x2 = '.' + x[1];
+  }
+  while (regex.test(x1)) {
+    x1 = x1.replace(regex, '$1' + ',' + '$2');
+  }
+
+  return x1 + x2;
+};
+
+/**
  * Get the value of a URL parameter
  *
  * @param name {String}
