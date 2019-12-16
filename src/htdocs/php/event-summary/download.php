c044746160ee715c5044a3eb4ad1e43e03a580dc
Scott Haefner <shaefner@usgs.gov>
2019-12-16 12:54:40 -0800

    minor tweaks

--- src/htdocs/php/event-summary/download.php
+++ src/htdocs/php/event-summary/download.php
@@ -1,18 +1,20 @@
 <?php
 
-downloadFile();
+downloadFile($_GET['file']);
 
 /**
- * Trigger download of RTF file created by rtf.php
+ * Trigger download of the (temporary) RTF file created by rtf.php
+ *
+ * @param file {String}
+ *     full path of RTF file
  */
-function downloadFile() {
-  $path = $_GET['file'];
-  $filename = preg_replace('/-\d+/', '', basename($path)); // filename sans path/timestamp
+function downloadFile($file) {
+  $filename = preg_replace('/-\d+/', '', basename($file)); // strip path/timestamp
 
   header('Content-Type: application/rtf');
   header('Content-Disposition: attachment; filename="' . $filename . '"');
-  header('Content-Length: ' . filesize($path));
+  header('Content-Length: ' . filesize($file));
   header('Connection: close');
 
-  readfile($path);
+  readfile($file);
 }
