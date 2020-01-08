ada718c9e5a5a97b69c295274d7613fa2c790f24
Scott Haefner <shaefner@usgs.gov>
2020-01-07 18:53:00 -0800

    create better error handling

--- src/htdocs/php/event-summary/create.php
+++ src/htdocs/php/event-summary/create.php
@@ -5,31 +5,50 @@ include_once 'Rtf.php';
 // Read in raw data from the Ajax request body and store it as an object
 $data = json_decode(file_get_contents('php://input'));
 
+$jsonParams = [
+  'error' => null,
+  'file' => null
+];
+
 // $file = fopen('/tmp/error.txt', 'w');
 // $output = print_r($data, true);
 // fwrite($file, $output);
 // fclose($file);
 
 // Create RTF file
-$rtf = new Rtf($data);
-
-sendResponse($rtf->file);
+try {
+  $rtf = new Rtf($data);
+  $jsonParams['file'] = $rtf->file;
+} catch (Exception $e) {
+  $jsonParams['error'] = $e->getMessage();
+} finally {
+  setHeaders($jsonParams);
+  sendResponse($jsonParams);
+}
 
 /**
  * Return response json, which contains the path to the temporary RTF file
  *
- * @param $file {String}
- *    full path of RTF file
+ * @param $params {Array}
  */
-function sendResponse($file) {
-  header('Content-Type: application/json');
-  header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Methods: *');
-  header('Access-Control-Allow-Headers: accept,origin,authorization,content-type');
-
-  $json = json_encode([
-    'file' => $file
-  ]);
+function sendResponse($params) {
+  $json = json_encode($params);
 
   print $json;
+}
+
+/**
+ * Set HTTP Headers for CORS-compatible json response
+ *
+ * @param $params {Array}
+ */
+function setHeaders($params) {
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Headers: Accept, Authorization, Content-Type, Origin');
+  header('Access-Control-Allow-Methods: *');
+  header('Access-Control-Allow-Origin: *');
+
+  if ($params['error']) {
+    header('HTTP/1.0 500 Internal Server Error');
+  }
 }
