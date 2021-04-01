f4ff4982a9a19d6fe54b6159a6d2dd920d0b6517
Haefner, Scott <shaefner@igswmhwglte0731.local>
2021-04-01 21:18:09 +0000

    reorder

--- grunt/config.js
+++ grunt/config.js
@@ -15,9 +15,9 @@ config = {
   dist: 'dist',
   distPort: basePort + 1,
   ini: ini.parse(fs.readFileSync('./src/conf/config.ini', 'utf-8')),
-  pkg: JSON.parse(fs.readFileSync('./package.json', 'utf-8')),
-  src: 'src',
   liveReloadPort: basePort + 9,
+  pkg: JSON.parse(fs.readFileSync('./package.json', 'utf-8')),
+  src: 'src'
 };
 
 
