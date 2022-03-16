7d9009efa4a24f43b058f744c12c553ca64293bb
Scott Haefner <4175182+shaefner-usgs@users.noreply.github.com>
2022-03-16 21:29:52 +0000

    replace deprecated FILTER_SANITIZE_STRING w/ strip_tags

--- src/htdocs/php/_functions.inc.php
+++ src/htdocs/php/_functions.inc.php
@@ -1,24 +1,20 @@
 <?php
 
 /**
- * Get a request parameter from $_GET or $_POST and sanitize it.
+ * Get a request parameter from $_GET or $_POST
  *
  * @param $name {String}
  *     The parameter name
  * @param $default {?} default is NULL
- *     Optional default value if the parameter was not provided.
- * @param $filter {PHP Sanitize filter} default is FILTER_SANITIZE_STRING
- *     Optional sanitizing filter to apply
+ *     Optional default value if the parameter was not set.
  *
  * @return $value {String}
  */
-function safeParam($name, $default=NULL, $filter=FILTER_SANITIZE_STRING) {
-  $value = NULL;
-
+function safeParam ($name, $default=NULL) {
   if (isset($_POST[$name]) && $_POST[$name] !== '') {
-    $value = filter_input(INPUT_POST, $name, $filter);
+    $value = strip_tags(trim($_POST[$name]));
   } else if (isset($_GET[$name]) && $_GET[$name] !== '') {
-    $value = filter_input(INPUT_GET, $name, $filter);
+    $value = strip_tags(trim($_GET[$name]));
   } else {
     $value = $default;
   }
