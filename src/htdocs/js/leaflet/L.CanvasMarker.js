8b57437067322997e2300bc7a3f093a22c81c56a
Scott Haefner <4175182+shaefner-usgs@users.noreply.github.com>
2022-05-13 21:39:42 +0000

    remove unnec. code; minor tweaks

--- src/htdocs/js/leaflet/L.CanvasMarker.js
+++ src/htdocs/js/leaflet/L.CanvasMarker.js
@@ -10,18 +10,10 @@
  * are turned on/off in the layers control.
  */
 L.Marker.CanvasMarker = L.Marker.extend({
-  initialize: function (latlng, options) {
-    L.Util.setOptions(this, options);
-    L.Marker.prototype.initialize.call(this, latlng, this.options);
-  },
-
   onAdd: function (map) {
-    var canvas,
-        className,
-        marker;
-
-    className = this.id;
-    canvas = document.querySelector('#mapPane canvas.' + className);
+    var marker,
+        className = this.id,
+        canvas = document.querySelector('#mapPane canvas.' + className);
 
     L.Marker.prototype.onAdd.call(this, map);
 
@@ -33,13 +25,9 @@ L.Marker.CanvasMarker = L.Marker.extend({
   },
 
   onRemove: function (map) {
-    var canvas,
-        className,
-        contentEl;
-
-    className = this.options.icon.options.className;
-    canvas = document.querySelector('.map canvas.' + className);
-    contentEl = document.querySelector('#mapPane .content');
+    var className = this.options.icon.options.className,
+        canvas = document.querySelector('.map canvas.' + className),
+        contentEl = document.querySelector('#mapPane .content');
 
     if (canvas) {
       contentEl.appendChild(canvas); // put <canvas> back
