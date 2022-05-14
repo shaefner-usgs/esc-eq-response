7f4de7aecf953dbb0d77058bc46b1b45336bf4ea
Scott Haefner <4175182+shaefner-usgs@users.noreply.github.com>
2022-05-14 00:05:51 +0000

    separate handlers into separate functions

--- src/htdocs/js/leaflet/L.EditableRectangle.js
+++ src/htdocs/js/leaflet/L.EditableRectangle.js
@@ -20,85 +20,77 @@ L.Control.EditableRectangle = L.Control.extend({
 
   initialize: function (options) {
     L.Util.setOptions(this, options);
+
+    this._defaultTitle = 'Create a new custom region';
+    this._instructions = document.querySelector('#searchBar .instructions');
+    this._region = options.region;
   },
 
   onAdd: function (map) {
-    var button,
-        container,
-        defaultTitle,
-        divClasses,
-        instructions,
-        newRegion,
-        region,
-        status;
+    var container,
+        divClasses;
 
-    defaultTitle = 'Create a new custom region';
     divClasses = [
       'leaflet-bar',
       'leaflet-control',
       'leaflet-control-edit'
     ];
     container = L.DomUtil.create('div', divClasses.join(' '));
-    button = L.DomUtil.create('a', '', container);
-    instructions = document.querySelector('#searchBar .instructions');
-    region = this.options.region;
 
-    button.href = '#';
-    button.innerHTML = '<div class="box"></div>';
-    button.title = defaultTitle;
+    this._button = L.DomUtil.create('a', '', container);
+    this._button.href = '#';
+    this._button.innerHTML = '<div class="box"></div>';
+    this._button.title = this._defaultTitle;
 
     map.on('editable:dragend editable:vertex:mousedown', () => {
       // Set a slight delay so Leaflet.Editable can update the bounds first
       setTimeout(this.options.app.SearchBar.setStatus, 500);
     });
+    map.on('editable:drawing:commit', this._onCommit, this);
 
-    map.on('editable:drawing:commit', e => {
-      var classList = e.originalEvent.target.classList;
-
-      // Ignore clicks on active control which also triggers a 'commit' event
-      if (!classList.contains('box') && !classList.contains('selected')) {
-        region = newRegion;
-        button.title = defaultTitle;
-
-        instructions.classList.add('hide');
-        button.classList.remove('selected');
-
-        this.options.app.SearchBar.setStatus();
-      }
-    });
-
-    L.DomEvent.on(button, 'click', e => {
-      L.DomEvent.stop(e);
-
-      // Stop Safari from registering two clicks, which effectively cancels button click
-      if (status === 'clicked') return;
-
-      status = 'clicked';
-      setTimeout(() => {
-        status = null;
-      }, 250);
-
-      instructions.classList.toggle('hide');
-      button.classList.toggle('selected');
-
-      if (button.classList.contains('selected')) {
-        newRegion = map.editTools.startRectangle();
-        button.title = 'Cancel and restore previous region';
-
-        map.removeLayer(region);
-      } else {
-        // Recreate cached region Rectangle b/c it loses its dragging ability
-        region = L.rectangle(region.getBounds());
-        button.title = defaultTitle;
-
-        map.editTools.stopDrawing();
-        map.removeLayer(newRegion);
-        map.addLayer(region);
-        region.enableEdit();
-      }
-    }, this);
+    L.DomEvent.on(this._button, 'click', this._onClick, this);
 
     return container;
+  },
+
+  _onClick: function () {
+    var selected;
+
+    this._button.classList.toggle('selected');
+    this._instructions.classList.toggle('hide');
+
+    selected = this._button.classList.contains('selected');
+
+    if (selected) {
+      this._button.title = 'Cancel and restore previous region';
+      this._newRegion = this._map.editTools.startRectangle();
+
+      this._map.removeLayer(this._region);
+    } else {
+      this._button.title = this._defaultTitle;
+
+      // Recreate cached region Rectangle b/c it loses its dragging ability
+      this._region = L.rectangle(this._region.getBounds());
+
+      this._map.editTools.stopDrawing();
+      this._map.removeLayer(this._newRegion);
+      this._map.addLayer(this._region);
+      this._region.enableEdit();
+    }
+  },
+
+  _onCommit: function (e) {
+    var classList = e.originalEvent.target.classList;
+
+    // Ignore clicks on active control which also triggers a 'commit' event
+    if (!classList.contains('box') && !classList.contains('selected')) {
+      this._button.title = this._defaultTitle;
+      this._region = this._newRegion;
+
+      this._button.classList.remove('selected');
+      this._instructions.classList.add('hide');
+      this.options.app.SearchBar.setStatus();
+    }
   }
 });
 
