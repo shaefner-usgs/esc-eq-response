365bd180bbe9b555ebbef255bfd6db644d883514
Scott Haefner <shaefner@igswmh246le1207.local>
2022-10-17 20:29:57 +0000

    incorporate latest code from Leaflet; add _override()

--- src/htdocs/js/leaflet/L.Popup.js
+++ src/htdocs/js/leaflet/L.Popup.js
@@ -10,35 +10,38 @@
 L.Popup.include({
   /**
    * Override _adjustPan from L.Popup.
-   *
-   * @param e {Event}
    */
-  _adjustPan: function (e) {
-    if (!this.options.autoPan || this._rendered) { return; }
+  _adjustPan: function () {
+    if (!this.options.autoPan) { return; }
     if (this._map._panAnim) { this._map._panAnim.stop(); }
 
-    var autoPanPadding = null,
+    // We can endlessly recurse if keepInView is set and the view resets.
+    // Let's guard against that by exiting early if we're responding to our own autopan.
+    if (this._autopanning) {
+      this._autopanning = false;
+      return;
+    }
+
+    var map = this._map,
         marginBottom = parseInt(L.DomUtil.getStyle(this._container, 'marginBottom'), 10) || 0,
         containerHeight = this._container.offsetHeight + marginBottom,
         containerWidth = this._containerWidth,
-        layerPos = new L.Point(this._containerLeft, -containerHeight - this._containerBottom),
-        map = this._map;
+        layerPos = new L.Point(this._containerLeft, -containerHeight - this._containerBottom);
 
     layerPos._add(L.DomUtil.getPosition(this._container));
 
-    if (Object.prototype.hasOwnProperty.call(this.options, 'autoPanPadding')) {
-      autoPanPadding = L.point(this.options.autoPanPadding);
-    }
-
     var containerPos = map.layerPointToContainerPoint(layerPos),
-        defaults = this._getDefaults(),
+        padding = L.point(this.options.autoPanPadding),
+        paddingTL = L.point(this.options.autoPanPaddingTopLeft || padding),
+        paddingBR = L.point(this.options.autoPanPaddingBottomRight || padding),
+        size = map.getSize(),
         dx = 0,
-        dy = 0,
-        paddingTL = L.point(this.options.autoPanPaddingTopLeft ||
-          autoPanPadding || defaults.topLeft),
-        paddingBR = L.point(this.options.autoPanPaddingBottomRight ||
-          autoPanPadding || defaults.bottomRight),
-        size = map.getSize();
+        dy = 0;
+
+    // Override
+    var override = this._override();
+    paddingTL = override.paddingTL;
+    paddingBR = override.paddingBR;
 
     if (containerPos.x + containerWidth + paddingBR.x > size.x) { // right
       dx = containerPos.x + containerWidth - size.x + paddingBR.x;
@@ -58,14 +61,15 @@ L.Popup.include({
     // @event autopanstart: Event
     // Fired when the map starts autopanning when opening a popup.
     if (dx || dy) {
+      // Track that we're autopanning, as this function will be re-ran on moveend
+      if (this.options.keepInView) {
+        this._autopanning = true;
+      }
+
       map
         .fire('autopanstart')
-        .panBy([dx, dy], {animate: e && e.type === 'moveend'});
+        .panBy([dx, dy]);
     }
-
-    // Flag that popup is already rendered to avoid autoPanning while it's open
-    this._rendered = true;
-    this._map.on('popupclose', this._setRendered, this);
   },
 
   /**
@@ -81,16 +85,31 @@ L.Popup.include({
 
     // Default values account for Header, SideBar and map controls
     return {
-      topLeft: new L.Point(left, top),
-      bottomRight: new L.Point(right, bottom),
+      paddingBR: L.point(right, bottom),
+      paddingTL: L.point(left, top)
     };
   },
 
   /**
-   * Event handler that resets _rendered to false.
+   * Override for _adjustPan.
+   *
+   * @return {Object}
    */
-  _setRendered: function () {
-    this._rendered = false;
-    this._map.off('popupclose', this._setRendered, this);
-  }
+  _override: function () {
+    var defaults = this._getDefaults(),
+        options = {
+          paddingBR: this.options.autoPanPaddingBottomRight,
+          paddingTL: this.options.autoPanPaddingTopLeft
+        },
+        padding = null;
+
+    if (Object.prototype.hasOwnProperty.call(this.options, 'autoPanPadding')) {
+      padding = L.point(this.options.autoPanPadding);
+    }
+
+    return {
+      paddingBR: L.point(options.paddingBR || padding || defaults.paddingBR),
+      paddingTL: L.point(options.paddingTL || padding || defaults.paddingTL)
+    };
+  },
 });
